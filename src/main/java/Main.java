91117d06ba0361c1d98cf897d8ec5b061b694c88
Brandon Barbour <brandon8barbour@gmail.com>
2021-06-13 14:54:33 -0400

    Final commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,19 +10,21 @@ public class Main {
         if (str1.length() != str2.length()) {
             return 0;
         } else {
+            char[] s1 = str1.toCharArray();
+            char[] s2 = str2.toCharArray();
             int check = 0;
-            for (int i = 0; i >= str1.length(); i++) {
-                for (int n = 0; n >= str1.length(); n++) {
-                    if (str1[i] = str2[n]) {
-                        check = 1;
-                        str2 = str2 - str2[n];
-                    }
-                    if (check == str1.length()) {
-                        return 1;
-                    } else {
-                        return 0;
+            for (int i = 0; i <= s1.length - 1; i++) {
+                for (int n = 0; n <= s2.length - 1; n++) {
+                    if (s1[i] == s2[n]) {
+                        check = check + 1;
+                        s2[n] = 0;
                     }
                 }
+            }
+            if (check == str1.length()) {
+                return 1;
+            } else {
+                return 0;
             }
         }
     }
@@ -30,9 +32,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Enter two strings and I'll tell you if they are anagrams:\n");
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter the first string:\n");
+        System.out.print("Enter the first string: ");
         String str1 = input.next();
-        System.out.print("Enter the second string:\n");
+        System.out.print("Enter the second string: ");
         String str2 = input.next();
         if (isAnagram(str1, str2) == 1) {
             System.out.printf("\"%s\" and \"%s\" are anagrams.", str1, str2);
@@ -40,6 +42,5 @@ public class Main {
         else {
             System.out.printf("\"%s\" and \"%s\" are not anagrams.", str1, str2);
         }
-
     }
 }
